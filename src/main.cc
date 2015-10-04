d3a7e8066721842708d9bdb9d699d0b90fbab63c
Nixsm <nichsmm@gmail.com>
2015-10-04 01:54:37 +0000

    working

--- src/main.cc
+++ src/main.cc
@@ -12,33 +12,40 @@ int main(int argc, char** argv){
     IO::readFile(width, height, map);
 
     std::shared_ptr<Matrix<char> > crossword = std::make_shared<Matrix<char> >(width, height);
-    std::shared_ptr<Matrix<bool> > visited = std::make_shared<Matrix<bool> >(width, height);
 
     for (auto i = 0; i < width; ++i) {
         for (auto j = 0; j < height; ++j) {
             crossword->set(i, j, map.at(i).at(j));
-            visited->set(i, j, false);
         }
     }
 
+    std::cout << *crossword << std::endl;
+    
+    unsigned int numThreads;
+
+    std::vector<std::string> words = {"threads", "arquivos", "sinais", "pipe", "processos", "mutex"};
+
+    std::vector<std::shared_ptr<Worker> > workers;
+    
+    do {
+        std::cout << "Numero de threads (Cada thread procura uma palavra)" << std::endl;
+        std::cin >> numThreads;
+    } while (!(numThreads < words.size() && numThreads > 0));
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    //Worker work("threads", crossword);
-    Worker work1("threads", crossword);
-    Worker work2("arquivos", crossword);
-    Worker work3("sinais", crossword);
-    Worker work4("pipe", crossword);
-    Worker work5("processos", crossword);
-    Worker work6("mutex", crossword);
+    for (auto i = 0u; i <= words.size() - numThreads; i+=numThreads) {
+        for (auto j = i; j < i + numThreads; ++j) {
+            workers.push_back(std::make_shared<Worker>(words.at(j), crossword));
+        }
 
-    work1.join();
-    work2.join();
-    work3.join();
-    work4.join();
-    work5.join();
-    work6.join();
+        
+        for (auto& worker : workers) {
+            worker->join();
+        }
 
+        workers.clear();
+    }
     
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
